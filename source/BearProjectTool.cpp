8ffc1fb444afa11e56f2af392d98cdadff8300bf
BearIvan <i-sobolevskiy@mail.ru>
2019-02-25 19:42:20 +0700

    Доработка Linux

---
+++ source/BearProjectTool.cpp
@@ -0,0 +1,7 @@
+#include "BearCore.hpp" 
+#ifdef WINDOWS
+#include "Windows/BearProjectTool_Windows.h"
+#elif LINUX
+#include "Linux/BearProjectTool_Linux.h"
+#endif 
+
---
+++ source/Windows/BearTimer_Windows.h
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <ctime>
+
+LARGE_INTEGER getFrequency()
+{
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+	return frequency;
+}
+BearCore::BearTime BearCore::BearTimer::GetCurrentTime() const
+{
+
+	HANDLE currentThread = GetCurrentThread();
+	DWORD_PTR previousMask = SetThreadAffinityMask(currentThread, 1);
+
+	static LARGE_INTEGER frequency = getFrequency();
+
+	LARGE_INTEGER time;
+	QueryPerformanceCounter(&time);
+
+	SetThreadAffinityMask(currentThread, previousMask);
+	
+	return /*BearCore::BearTime(int32(std::clock()));*/BearTime(1000000 * time.QuadPart / frequency.QuadPart);
+}
